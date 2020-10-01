b0fb4dc85c143d1e5b8bc398c5df3a38d5d894fe
Rui <ruimobile2012@gmail.com>
2020-10-01 00:41:03 -0700

    validator for update implemented.

--- index.js
+++ index.js
@@ -73,7 +73,8 @@ app.put('/api/persons/:id', (req, res, next) => {
         number: body.number,
     }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    Person.findByIdAndUpdate(req.params.id, person,
+        { new: true, runValidators: true })
         .then(updatedPerson => res.json(updatedPerson))
         .catch(err => next(err))
 })
